3e29c9e9cad68e88e3405179a8fad14d7084b988
Naren-Raju Nagarajan <ssp201718@ssp18.asiaa.sinica.edu.tw>
2017-07-19 15:45:35 +0800

    New SQL Code added to access HSC Data.

---
+++ bubbleimg/obsobj/hsc/photoObj.sql
@@ -0,0 +1,22 @@
+SELECT
+  main.object_id,
+  main.ra, main.dec,
+  main.patch_id,
+  main.tract,
+  main.patch,
+  main.patch_s,
+  main.parent_id,
+  main.deblend_nchild,
+  main.detect_is_patch_inner,
+  main.detect_is_tract_inner,
+  main.detect_is_primary,
+  {sqlcolumns}
+
+FROM
+  {rerun}.forced AS main
+
+WHERE
+  (main.detect_is_tract_inner = 't' 
+   AND main.detect_is_patch_inner = 't' 
+   AND main.detect_is_primary = 't')
+   AND main.object_id = {object_id}  
