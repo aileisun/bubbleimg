926b924c980eb6577ba6fcbb9a1ad54fe252ab8c
Sun Ai Lei <aisun@Ai-Leis-MacBook-Pro.local>
2017-10-05 16:23:36 -0400

    move old code under deprecated

--- bubbleimg/contaminants/__init__.py
+++
@@ -1,9 +0,0 @@
-# __init__.py
-
-__all__ = ['contaminants']
-
-import contaminants
-reload(contaminants)
-
-from contaminants import dir_find_contaminants
-from contaminants import dir_reduce_contaminantcount
